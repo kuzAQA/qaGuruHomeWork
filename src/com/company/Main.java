08cbeec932a89f0cfce80db4c1c5b1d8e80c4e73
kuzAQA <101124480+kuzAQA@users.noreply.github.com>
2022-03-30 14:18:44 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Start homework 2");
-        System.out.println("Подготовка почвы для конфликта в новой ветке");
+        System.out.println("Мучу кручу конфликтики");
     }
 }
